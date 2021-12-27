0cdf8a9003a409915a6679c799e105da205816f8
scottsuk0306 <scottsuk0306@kaist.ac.kr>
2021-12-28 00:17:51 +0900

    Create repository

--- test.py
+++ test.py
@@ -19,10 +19,22 @@ print("token: " + token)
 
 folderName = "test-project"
 target_path = os.path.join(path, folderName)
+print("target_path: " + target_path)
 os.makedirs(target_path, exist_ok=True)
 
+projectName = folderName
+
 g = Github(token)
-for repo in g.get_user().get_repos():
-    print(repo.name)
+user = g.get_user()
+
+try:
+    user.create_repo(projectName)
+except github.GithubException.GithubException:
+repo = user.create_repo(projectName)
+print("Succesfully created repository {}".format(folderName))
+
+
+# for repo in g.get_user().get_repos():
+#    print(repo.name)
 
 # print("Succesfully created repository {}".format(folderName))
