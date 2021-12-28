78eae92a2f42def0a3a459fba141a039242e4d75
scottsuk0306 <scottsuk0306@kaist.ac.kr>
2021-12-28 23:25:12 +0900

    Generate README.md and .gitignore

--- .my_commands.sh
+++ .my_commands.sh
@@ -8,6 +8,7 @@ function create() {
     git init
     git remote add origin git@github.com:$USERNAME/$1.git
     touch README.md
+    touch .gitignore
     git add .
     git commit -m "Initial commit"
     git push -u origin master
