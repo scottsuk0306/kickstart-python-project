cd75a42bca0bf8f65d7270c322057222d1cf7d1c
scottsuk0306 <scottsuk0306@kaist.ac.kr>
2021-12-29 23:04:51 +0900

    Test some shell scripts

---
+++ start_project.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cd
+source .env
+python create.py $1
+# cd $FILEPATH$1
+# git init
+# git remote add origin git@github.com:$USERNAME/$1.git
+# touch README.md
+# touch .gitignore
+# git add .
+# git commit -m "Initial commit"
+# git push -u origin master
+# code .
